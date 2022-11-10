300ea81b7e2d74a2e8cd7d6e23cb91b6159015e1
Perry Mitchell <perry@perrymitchell.net>
2022-11-10 19:15:08 +0200

    Fix publishing w/ version tag

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ VERSION_TAG=$(git tag -l --sort=-creatordate | head -n 1)
 echo " • App version: ${VERSION_TAG}"
 
 DOCKER_PREFIX="perrymitchell/nextcloud-directory-sync"
-export TAG_VER=$DOCKER_PREFIX:v${VERSION_TAG}
+export TAG_VER=$DOCKER_PREFIX:${VERSION_TAG}
 export TAG_LATEST=$DOCKER_PREFIX:latest
 
 echo " • Building"
