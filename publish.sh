d4bd5e1cf403f79135569c665d78e8cf6daea67d
Perry Mitchell <perry@perrymitchell.net>
2022-11-10 19:08:15 +0200

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Building and deploying docker image";
+
+VERSION_TAG=$(git tag -l --sort=-creatordate | head -n 1)
+echo " • App version: ${VERSION_TAG}"
+
+DOCKER_PREFIX="perrymitchell/nextcloud-directory-sync"
+export TAG_VER=$DOCKER_PREFIX:v${VERSION_TAG}
+export TAG_LATEST=$DOCKER_PREFIX:latest
+
+echo " • Building"
+docker build -t $TAG_VER -t $TAG_LATEST .
+
+echo " • Pushing"
+docker push $TAG_VER
+docker push $TAG_LATEST
+
+echo "Done"
+
+cd -
